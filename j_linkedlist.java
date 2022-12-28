e9773d14ed6dc7ee524b92a40ebbf26cb011a401
jetblacksavior <coolanimeguy666@gmail.com>
2022-12-27 20:53:46 -0700

    added stutfff

--- j_linkedlist.java
+++ j_linkedlist.java
@@ -13,14 +13,7 @@ class j_linkedlist{
         arguements.add(10);
         arguements.add(50);
         linkedlist = new List<Integer>(arguements);
-        // List<Integer>.Node temp_ptr = linkedlist.root;
-        // while(temp_ptr != null){
-        //     System.out.println("my data is "+temp_ptr.data);
-        //     System.out.println("my class data is "+temp_ptr);
-
-        //     temp_ptr = temp_ptr.next;
-        // }   
-        ////for some reason it's printing out in reverse?
+ 
         for( var x : linkedlist.getElems()){
             System.out.println(x + "is the value");
         }
